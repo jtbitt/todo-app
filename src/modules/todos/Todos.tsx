9b4d874fb5b619e4d15adf8553a872e29d5adacd
Jay Bittner <jtbitt@gmail.com>
2021-04-15 19:39:55 -0500

    Added some error handling for api call

--- src/modules/todos/Todos.tsx
+++ src/modules/todos/Todos.tsx
@@ -1,19 +1,35 @@
 import { useState, useEffect } from "react";
 
 export const Todos = () => {
+  const [error, setError] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/todos"
-      );
-      const data = await response.json();
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/todos"
+        );
+        const data = await response.json();
 
-      setTodos(data);
+        setTodos(data);
+      } catch (e) {
+        setError(e.message || "Something went wrong");
+      }
+
+      setIsLoading(false);
     };
     fetchData();
   }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
 
   return (
     <ul>
