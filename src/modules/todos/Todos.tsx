a4747954a0c19e5265f7e6295d42e85d39c24177
Jay Bittner <jtbitt@gmail.com>
2021-04-15 19:30:50 -0500

    Getting the basic data pulled in with fetch

--- src/modules/todos/Todos.tsx
+++ src/modules/todos/Todos.tsx
@@ -1,3 +1,25 @@
+import { useState, useEffect } from "react";
+
 export const Todos = () => {
-  return <div>Hi</div>;
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+      const data = await response.json();
+
+      setTodos(data);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <ul>
+      {todos.map(({ title, id }) => (
+        <li key={id}>{title}</li>
+      ))}
+    </ul>
+  );
 };
