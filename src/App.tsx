26c74328a745e96e2e3003d1688e45d016b44c2b
Jay Bittner <jtbitt@gmail.com>
2021-10-18 19:59:47 -0500

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./App.css";
 
 import { Routes } from "./Routes";
